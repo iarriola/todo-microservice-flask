13a48ba23ce16b50cffe29155e0334bc75c9f05e
Ivan Arriola <iarriolar@gmail.com>
2023-05-05 00:15:11 -0600

    Working on repository structure

--- src/main.py
+++ src/main.py
@@ -1,21 +1,13 @@
-import os
-import psycopg2
-from dotenv import load_dotenv
-
 import json
 from flask import Flask, jsonify, request, make_response, redirect, render_template
-
-load_dotenv()
+from repositories.task_repository import TaskRepository
 
 app = Flask(__name__)
 
-DB_URL = os.getenv("DATABASE_URL")
-CONNECION = psycopg2.connect(DB_URL)
+repository = TaskRepository()
 
 TASK_ROUTE = "/api/v1/tasks/:id"
 TASKS_ROUTE = "/api/v1/tasks"
-
-# select id, uuid, title, description, created_at, completed_at, deleted_at from task;
 
 
 @app.route(TASK_ROUTE, methods=["GET"])
@@ -23,30 +15,10 @@ def get():
     return jsonify({"name": "alice", "email": "alice@outlook.com"})
 
 
-SELECT_ALL = "select uuid, title, description, created_at, completed_at, deleted_at from todo.task;"
-
-
 @app.route(TASKS_ROUTE, methods=["GET"])
 def get_all():
-    with CONNECION.cursor() as cursor:
-        cursor.execute(SELECT_ALL)
-        tasks = cursor.fetchall()
-        if tasks:
-            result = []
-            for task in tasks:
-                result.append(
-                    {
-                        "id": task[0],
-                        "title": task[1],
-                        "description": task[2],
-                        "createdAt": task[3],
-                        "completedAt": task[4],
-                        "deletedAt": task[5],
-                    }
-                )
-            return result, 200
-        else:
-            return jsonify([]), 200
+    result = repository.get_all()
+    return jsonify(result), 200
 
 
 @app.route(TASKS_ROUTE, methods=["POST"])
